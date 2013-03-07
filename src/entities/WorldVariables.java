df3636a3e7605a5c6c156feb34c2b765a0741a1b
wagner.franchin <wagner.franchin@60684bb1-9e4d-4637-9da6-14ecf1662cbe>
2013-03-07 16:09:50 +0000

    -Adição do package "sprites"; -Criação da classe SpriteSheet; -Seperação em 2 spritesheets: entidades e explosao;

--- src/entities/WorldVariables.java
+++ src/entities/WorldVariables.java
@@ -1,14 +1,6 @@
 package entities;
 
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.JDOMException;
-import org.jdom2.input.SAXBuilder;
-
-import static org.lwjgl.opengl.GL11.*;
-
-import java.io.IOException;
-import java.util.HashMap;
+import sprites.SpriteSheet;
 
 public final class WorldVariables {
 	public static int WIDTH = 640;
@@ -16,33 +8,11 @@ public final class WorldVariables {
 	public static int IMAGE_WIDTH = 32;
 	public static int IMAGE_HEIGHT = 32;
 	
-	public static int spritesheet;
-	public static HashMap<String, Sprite> spriteMap = new HashMap<String, Sprite>();
+	public static SpriteSheet moveableEntities; 
+	public static SpriteSheet explosion; 
 	
 	public static void setUpSpriteSheet() {
-		spritesheet = ImagingTools.glLoadTextureLiner("res/images/spritesheet.png");
-		SAXBuilder builder = new SAXBuilder();
-		
-		try {
-			
-			Document document = builder.build("res/images/spritesheet.sprites");
-			Element root = document.getRootElement();
-			for (Object spriteObject : root.getChildren()) {
-				Element spriteElement = (Element) spriteObject;
-				String name = spriteElement.getAttributeValue("name");
-				int x = spriteElement.getAttribute("x").getIntValue();
-				int y = spriteElement.getAttribute("y").getIntValue();
-				int w = spriteElement.getAttribute("w").getIntValue();
-				int h = spriteElement.getAttribute("h").getIntValue();
-				Sprite sprite = new Sprite(name, x, y, w, h);
-				spriteMap.put(sprite.getName(), sprite);
-			}
-			
-		} catch (JDOMException e) {
-			glDeleteTextures(spritesheet);
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		moveableEntities = new SpriteSheet("res/images/spriteSheetEntities.png", "res/images/spriteSheetEntities.sprites");
+		explosion = new SpriteSheet("res/images/explosion/spriteSheetExplosion.png", "res/images/explosion/spriteSheetExplosion.sprites");
 	}
 }
---
+++ src/sprites/SpriteSheet.java
@@ -0,0 +1,55 @@
+package sprites;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+
+import static org.lwjgl.opengl.GL11.*;
+
+public class SpriteSheet {
+
+	private String file_png;
+	private String file_sprites;
+	
+	public int spriteSheet;
+	public HashMap<String, Sprite> spriteMap = new HashMap<String, Sprite>();
+	
+	public SpriteSheet(String file_png, String file_sprites) {
+		this.file_png = file_png;
+		this.file_sprites = file_sprites;
+		
+		setUpSpriteSheet();
+	}
+
+	private void setUpSpriteSheet() {
+		spriteSheet = ImagingTools.glLoadTextureLiner(file_png);
+		SAXBuilder builder = new SAXBuilder();
+		
+		try {
+			
+			Document document = builder.build(file_sprites);
+			Element root = document.getRootElement();
+			for (Object spriteObject : root.getChildren()) {
+				Element spriteElement = (Element) spriteObject;
+				String name = spriteElement.getAttributeValue("name");
+				int x = spriteElement.getAttribute("x").getIntValue();
+				int y = spriteElement.getAttribute("y").getIntValue();
+				int w = spriteElement.getAttribute("w").getIntValue();
+				int h = spriteElement.getAttribute("h").getIntValue();
+				Sprite sprite = new Sprite(name, x, y, w, h);
+				spriteMap.put(sprite.getName(), sprite);
+			}
+			
+		} catch (JDOMException e) {
+			glDeleteTextures(spriteSheet);
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
