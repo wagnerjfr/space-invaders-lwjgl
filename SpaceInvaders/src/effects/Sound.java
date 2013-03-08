6d8a2c62ffa2ef9a3d744d0cb1a455b1d6ca9e61
wagner.franchin <wagner.franchin@60684bb1-9e4d-4637-9da6-14ecf1662cbe>
2013-03-08 23:28:45 +0000

    -Eliminacao da classe SoundManager (que faz uso da lib do lwjgl para criar os sons) e criacao da classe Sound (que gerencia a criacao de sons com a lib Slick). Foi alterado porque, apos determinado tempo jogando, os sons paravam de ser emitidos. Agora parece que esta corrigido o problema.

---
+++ SpaceInvaders/src/effects/Sound.java
@@ -0,0 +1,46 @@
+package effects;
+
+import java.io.IOException;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.openal.AL;
+import org.newdawn.slick.openal.Audio;
+import org.newdawn.slick.openal.AudioLoader;
+import org.newdawn.slick.openal.SoundStore;
+import org.newdawn.slick.util.ResourceLoader;
+
+public class Sound {
+	
+	private Audio wavEffect;
+	public Sound(SoundType type) {
+		init(type);
+	}
+	
+	public static void create() {
+		try {
+			AL.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void init(SoundType type) {
+		try {
+			wavEffect = AudioLoader.getAudio("WAV", ResourceLoader.getResourceAsStream(type.location));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void play() {
+		wavEffect.playAsSoundEffect(1.0f, 1.0f, false);
+		
+		// polling is required to allow streaming to get a chance to
+		// queue buffers.
+		SoundStore.get().poll(0);
+	}
+	
+	public static void destroy() {
+		AL.destroy();
+	}
+}
